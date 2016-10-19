9a2926f030b4844335edba687b991b5044ce6673
Yogan  S <844015@India.TCS.com>
2016-10-19 13:00:51 +0530

    "ngRoute code changes"

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,54 @@
 // app "One University Exam Results" declaration
 
-var univExamResults=angular.module('univExamResults',[]);
+var univExamResults=angular.module('univExamResults',['ngRoute']);
+
+
+// ngRoute configuration
+
+univExamResults.config(function($routeProvider) {
+  $routeProvider
+  
+  .when('/',{
+    templateUrl : 'Pages/home.html'
+  })
+  
+  .when('/Results',{
+    templateUrl : 'Pages/Results.html',
+    controller : 'ResultsCtrl'
+  })
+  
+  .when('/About',{
+    templateUrl : 'Pages/About.html',
+  })
+  
+  .otherwise('/',{
+    templateUrl : 'Pages/home.html'
+  })
+  
+});
+
+//menulist directive definition
+
+univExamResults.directive('menulist',function(){
+  
+  var directive = {};
+  
+  directive.restrict = 'E';
+  
+  directive.template = '<ul class="nav navbar-nav"><li ng-repeat="menuItem in menuItems"><a href="#{{ menuItem}}"> {{ menuItem }} </a><li></ul>';
+
+  return directive;
+  
+});
+
 
 // Home page controller definition
 
 univExamResults.controller('HomePageCtrl', function HomePageCtrl($scope) {
   $scope.menuItems=[
     "Home",
-    "View Result",
-    "About Us",
-    "Contact"
+    "Results",
+    "About"
     ];
 });
 
